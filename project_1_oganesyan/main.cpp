e152c025d6d47530803950996fbd0c737e216f29
anneett <ann_oganesyan59@mail.ru>
2023-09-07 14:01:58 +0300

    new_cod

--- project_1_oganesyan/main.cpp
+++ project_1_oganesyan/main.cpp
@@ -1,9 +1,13 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
 int main()
 {
+	string symb;
 	cout << "Hello World!" <<  endl;
+	cout << "Enter symbol:";
+	cin >> symb;
+	cout << "Entered symbol: " << symb << endl;
 	return 0;
 }
